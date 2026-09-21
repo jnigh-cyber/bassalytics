e120ec930a74691f6622ea5a4a037df8f36ddf7e
Jacob <erfnewt@gmail.com>
2026-09-21 10:32:07 -0400

    feat: fetch and display current weather with hardcoded lake.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
-
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
+import { useCurrentWeather } from '../hooks/useCurrentWeather';
+import { describeWeatherCodes } from '../lib/describeweathercodes'
+import { cToF } from '../lib/units'
+ 
 export default function Index() {
+
+  const { data, isPending, error } = useCurrentWeather();
+
+  if(isPending) return <ActivityIndicator />
+
+  if(error) return <Text>Error: {error.message}</Text>
+
+  if(!data) return <Text>No data.</Text>
 
   return (
     <View style={styles.container}>
-      <Text>Testing text here.</Text>
+      <Text>{data.current.temperature_2m}°</Text>
+      <Text>{describeWeatherCodes(data.current.weather_code)}</Text>
     </View>
   );
 }
@@ -12,7 +24,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
